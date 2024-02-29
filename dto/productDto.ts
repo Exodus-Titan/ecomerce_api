eb0010f5cc82b607bee6e7d73b73ba1b54d6323c
Carlos Ortega <carloseduardortega@gmail.com>
2024-02-28 22:16:25 -0400

    revert: :rewind: reverted last commit

--- dto/productDto.ts
+++ dto/productDto.ts
@@ -16,13 +16,13 @@ export class ProductDto{
   stock: number;
 
   @IsString()
-  categoryName: string;
+  categoryId: string;
 
-  constructor(name: string, description: string, price: number, stock: number, categoryName: string){
+  constructor(name: string, description: string, price: number, stock: number, categoryId: string){
     this.name = name;
     this.description = description;
     this.price = price;
     this.stock = stock;
-    this.categoryName = categoryName;
+    this.categoryId = categoryId;
   }
 }
